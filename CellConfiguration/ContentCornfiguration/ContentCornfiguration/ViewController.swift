4bb8096ffd627fecf7acb4c97358e715107464ab
MarkD <markd@borkware.com>
2022-02-09 18:41:46 -0500

    adds content configuration test pröject

---
+++ CellConfiguration/ContentCornfiguration/ContentCornfiguration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContentCornfiguration
+//
+//  Created by Mark Dalrymple on 2/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
