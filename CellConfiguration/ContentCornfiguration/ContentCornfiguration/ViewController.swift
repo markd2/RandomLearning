97db3ef9a412edad24de902fe06c2c825d960ba9
MarkD <markd@borkware.com>
2022-02-09 19:01:50 -0500

    Simple old-school tableview

--- CellConfiguration/ContentCornfiguration/ContentCornfiguration/ViewController.swift
+++ CellConfiguration/ContentCornfiguration/ContentCornfiguration/ViewController.swift
@@ -1,19 +1,27 @@
-//
-//  ViewController.swift
-//  ContentCornfiguration
-//
-//  Created by Mark Dalrymple on 2/9/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "SNORGLE")
     }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 42
+    }
+    
+    func tableView(_ tableView: UITableView, 
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SNORGLE", for: indexPath)
+        cell.textLabel?.text = "\(indexPath)"
+        
+        return cell
+    }
 }
 
