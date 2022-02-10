295bc2db8ecce817da618ec376d819c318b518ab
MarkD <markd@borkware.com>
2022-02-09 20:46:17 -0500

    Adds a custom configuration pair.  That was... pretty easy.

--- CellConfiguration/ContentCornfiguration/ContentCornfiguration/ViewController.swift
+++ CellConfiguration/ContentCornfiguration/ContentCornfiguration/ViewController.swift
@@ -19,9 +19,70 @@ extension ViewController: UITableViewDataSource {
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cell = tableView.dequeueReusableCell(withIdentifier: "SNORGLE", for: indexPath)
-        cell.textLabel?.text = "\(indexPath)"
+
+        if indexPath.row % 2 == 0 {
+            cell.textLabel?.text = "\(indexPath)"
+        } else {
+            let config = SnorgleContentConfiguration(text: "hello \(indexPath)")
+            cell.contentConfiguration = config
+        }
         
         return cell
     }
 }
 
+// --------------------------------------------------
+
+class SnorgleView: UIView, UIContentView {
+    let label: UILabel
+
+    var configuration: UIContentConfiguration {
+        didSet {
+            configure(configuration: configuration)
+        }
+    }
+
+    init(_ configuration: UIContentConfiguration) {
+        self.configuration = configuration
+        label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        super.init(frame: .zero)
+
+        // add subviews
+        addSubview(label)
+
+        let constraints = [
+          leftAnchor.constraint(equalTo: label.leftAnchor),
+          topAnchor.constraint(equalTo: label.topAnchor),
+          bottomAnchor.constraint(equalTo: label.bottomAnchor),
+          rightAnchor.constraint(equalTo: label.rightAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+        
+        configure(configuration: configuration)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) get bent")
+    }
+    
+    func configure(configuration: UIContentConfiguration) {
+        guard let configuration = configuration as? SnorgleContentConfiguration else { return }
+        label.text = configuration.text
+    }
+
+}
+
+struct SnorgleContentConfiguration: UIContentConfiguration {
+    var text: String
+
+    func makeContentView() -> UIView & UIContentView {
+        return SnorgleView(self)
+    }
+    
+    func updated(for state: UIConfigurationState) -> SnorgleContentConfiguration {
+        return self
+    }
+    
+
+}
