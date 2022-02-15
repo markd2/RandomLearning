c7966f35cb966f6580e726ecfd674a7450a0f0bc
markd2 <mdalrymple@etsy.com>
2022-02-15 10:27:04 -0500

    checkpoint

--- RelativeDateTimeFormatter/README.md
+++ RelativeDateTimeFormatter/README.md
@@ -3,3 +3,8 @@
 Needed to do a "two days ago", "yesterday", etc.  RelativeDateTimeFormatter
 is the tool.
 
+* https://developer.apple.com/documentation/foundation/relativedatetimeformatter
+  - "A formatter that create local-aware string representations of a relative date or time"
+  - Use as stand-o-lone strings, like "1 hour ago", "in 2 weeks", etc.  Embedding
+    them in strings may grammatically incorrect be doing.
+
--- RelativeDateTimeFormatter/blah.swift
+++ RelativeDateTimeFormatter/blah.swift
@@ -1,3 +1,31 @@
 #!/usr/bin/swift
 
-print("ook")
+import Foundation
+
+let formatter = RelativeDateTimeFormatter()
+
+let date1 = 0.dateFrom
+let hundredSecondsFromNow = 100.dateFrom
+
+let blah = formatter.localizedString(for: hundredSecondsFromNow,
+                                     relativeTo: date1)
+print(blah)
+
+
+
+extension Double {
+    /// Date from self-seconds since reference date
+    /// 0.dateFrom
+    var dateFrom: Date {
+        Date(timeIntervalSinceReferenceDate: TimeInterval(self))
+    }
+}
+
+extension Int {
+    /// Date from self-seconds since reference date
+    /// 0.dateFrom
+    var dateFrom: Date {
+        Date(timeIntervalSinceReferenceDate: TimeInterval(self))
+    }
+}
+
