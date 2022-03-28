c111fb2afe66c455feb1ccacbecb7f537f85e1cc
MarkD <markd@borkware.com>
2022-03-27 20:10:38 -0400

    part-way there. calculus flashbacks =:-O

--- codewars/tortoise-racing.swift
+++ codewars/tortoise-racing.swift
@@ -16,13 +16,53 @@
 
 import Foundation
 
-func race(_ v1: Int, _ v2: Int, _ headstart: Int) -> [Int]? {
-    return [0, 0, 0]
+// slow/fast are in feet per hour
+func race(_ slow: Int, _ fast: Int, _ headstart: Int) -> [Int]? {
+    var slowPos = slow
+    var fastPos = fast
+
+    var accumulator = Double(headstart) / Double(slow)
+
+    // how long it takes fast to get to headstart (hours)
+    func catchup(_ fast: Int, _ headstart: Int) -> Double {
+        return Double(headstart) / Double(fast)
+    }
+    
+    let time = catchup(fast, headstart)
+    slowPos = headstart
+
+    print("time \(time)")
+    accumulator += time
+
+    var newDistance = Double(slow) * time
+    fastPos = Int(newDistance)
+    
+    print("\(slowPos) vs \(fastPos)")
+
+    while newDistance > 0 {
+        let newTime = catchup(fast, Int(newDistance))
+        print("time \(newTime)")
+        accumulator += newTime
+
+        newDistance = Double(slow) * newTime
+        slowPos = fastPos
+        fastPos = Int(newDistance)
+        print("\(slowPos) vs \(fastPos)")
+    }
+    print("new distance is \(newDistance)")
+    print("accumulator is \(accumulator)")
+
+    let floatSeconds = accumulator * 60 * 60
+    let hour = Int(floatSeconds) / (60 * 60)
+    let minute = (Int(floatSeconds) - (hour * 60*60)) / 60
+    let seconds = Int(floatSeconds) % 60
+
+    return [hour, minute, seconds]
 }
 
 let testcases: [((Int, Int, Int), (Int, Int, Int))] = [
   ((720, 850, 70), (0, 32, 18)),
-  ((80, 91, 37), (3, 21, 49))
+//  ((80, 91, 37), (3, 21, 49))
 ]
 
 var failwaffle = false
