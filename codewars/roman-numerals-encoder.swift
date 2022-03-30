dae5aee51e40239e646f617e5e52bade2129acdd
MarkD <markd@borkware.com>
2022-03-29 21:27:30 -0400

    adds failing test

---
+++ codewars/roman-numerals-encoder.swift
@@ -0,0 +1,51 @@
+#!/usr/bin/swift
+
+// https://www.codewars.com/kata/51b62bf6a9c58071c600001b/train/swift
+
+import Foundation
+
+// convert a number to its roman numeral representation
+// "remember there can't be more than three identical symbols in a row"
+
+func solution(_ number:Int) -> String {
+    return ""
+}
+
+let testcases: [(Int, String)] = [
+  (1, "I"),
+  (2, "II"),
+  (3, "III"),
+  (4, "IV"),
+  (5, "V"),
+  (6, "VI"),
+  (7, "VII"),
+  (8, "VIII"),
+  (9, "IX"),
+  (10, "X"),
+  (100, "C"),
+  (444, "CDXLIV"),
+  (1000, "M"),
+  (1954, "MCMLIV"),
+  (1990, "MCMXC"),
+  (1999, "MCMXCIX"),
+  (2000, "MM"),
+  (2008, "MMVIII"),
+  (3000, "MMM"),
+  (3900, "MMMCM"),
+  (3914, "MMMCMXIV")
+]
+
+var failwaffle = false
+for kase in testcases {
+    let mcmxviii = solution(kase.0)
+    if mcmxviii != kase.1 {
+        failwaffle = true
+        print("for \(kase.0) got \(mcmxviii) expected \(kase.1)")
+    }
+}
+
+if !failwaffle {
+    print("success")
+}
+
+
