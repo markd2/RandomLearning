bf895b130a14a63e07f9cd57fef47eaf10904f3d
MarkD <markd@borkware.com>
2022-12-26 10:22:37 -0500

    Adds sample Mac OS template apps

---
+++ DataOrientedDesign/DungeonDoD/DungeonDoD/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DungeonDoD
+//
+//  Created by Mark Dalrymple on 12/26/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
