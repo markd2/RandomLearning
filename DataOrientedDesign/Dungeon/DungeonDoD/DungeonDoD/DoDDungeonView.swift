3db58ac00c3215bf3f5c0a8d49b802d464aee8e5
MarkD <markd@borkware.com>
2022-12-27 20:00:06 -0500

    Draws rooms!

--- DataOrientedDesign/Dungeon/DungeonDoD/DungeonDoD/DoDDungeonView.swift
+++ DataOrientedDesign/Dungeon/DungeonDoD/DungeonDoD/DoDDungeonView.swift
@@ -15,6 +15,23 @@ class DoDDungeonView: NSView {
             NSBezierPath.stroke(rect)
         }
 
+        dungeon.rooms.storage.forEach { room in
+            drawRoom(room)
+        }
+
     }
+
+    func drawRoom(_ room: Room) {
+        NSColor.white.set()
+        NSBezierPath.fill(room.bounds)
+        NSColor.black.set()
+        NSBezierPath.stroke(room.bounds)
+
+        let titleRect = room.bounds.insetBy(dx: 5, dy: 1)
+        (room.name as NSString).draw(in: titleRect)
+    }
+
+    // we're aflippa da bitz
+    override var isFlipped: Bool { return true }
 }
 
