2a7f30a1acf5f5c4b3e41a7159682e0c6b279249
MarkD <markd@borkware.com>
2023-01-15 19:53:00 -0500

    Adds interpreter calling and error reporting

--- CraftingInterpreters/jlox/com/borkware/lox/Lox.java
+++ CraftingInterpreters/jlox/com/borkware/lox/Lox.java
@@ -11,7 +11,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -28,6 +30,7 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -61,6 +64,8 @@ public class Lox {
 
         // print out the AST
         System.out.println(new AstPrinter().print(expression));
+
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -73,6 +78,12 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    public static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                           "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
