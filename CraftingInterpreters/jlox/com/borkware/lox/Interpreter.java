acb4ca3441b97f79a3f69e61832334391cf23c75
MarkD <markd@borkware.com>
2023-01-15 20:02:34 -0500

    Adds (challenge) division by zero check.

--- CraftingInterpreters/jlox/com/borkware/lox/Interpreter.java
+++ CraftingInterpreters/jlox/com/borkware/lox/Interpreter.java
@@ -61,6 +61,9 @@ class Interpreter implements Expr.Visitor<Object> {
             throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings");
         case SLASH:
             checkNumberOperands(expr.operator, left, right);
+            if ((double)right == 0.0) {
+                throw new RuntimeError(expr.operator, "Attempted division by zero. Please don't do that");
+            }
             return (double)left / (double)right;
         case STAR:
             checkNumberOperands(expr.operator, left, right);
