056706ee61e3bcd86b03330c8ab8c3260dc60b93
MarkD <markd@borkware.com>
2023-02-05 21:17:58 -0500

    Finished with properties

--- CraftingInterpreters/jlox/com/borkware/lox/Interpreter.java
+++ CraftingInterpreters/jlox/com/borkware/lox/Interpreter.java
@@ -61,6 +61,16 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return null;
     }
     
+    @Override
+    public Object visitGetExpr(Expr.Get expr) {
+        Object object = evaluate(expr.object);
+        if (object instanceof LoxInstance) {
+            return ((LoxInstance) object).get(expr.name);
+        }
+
+        throw new RuntimeError(expr.name, "Only instances have properties");
+    }
+
     void executeBlock(List<Stmt> statements, Environment environment) {
         Environment previous = this.environment;
         try {
@@ -189,6 +199,18 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         }
 
         return evaluate(expr.right);
+    }
+
+    @Override
+    public Object visitSetExpr(Expr.Set expr) {
+        Object object = evaluate(expr.object);
+        
+        if (!(object instanceof LoxInstance)) {
+            throw new RuntimeError(expr.name, "only instances have fields");
+        }
+        Object value = evaluate(expr.value);
+        ((LoxInstance)object).set(expr.name, value);
+        return value;
     }
     
     @Override
