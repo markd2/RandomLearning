71a90e441732718784abf523504965acd9130e65
MarkD <markd@borkware.com>
2023-02-12 11:28:01 -0500

    Wraps up JavaLox™

--- CraftingInterpreters/jlox/com/borkware/lox/LoxClass.java
+++ CraftingInterpreters/jlox/com/borkware/lox/LoxClass.java
@@ -5,9 +5,12 @@ import java.util.Map;
 
 class LoxClass implements LoxCallable {
     final String name;
+    final LoxClass superclass;
     private final Map<String, LoxFunction> methods;
 
-    LoxClass(String name, Map<String, LoxFunction> methods) {
+    LoxClass(String name, LoxClass superclass,
+             Map<String, LoxFunction> methods) {
+        this.superclass = superclass;
         this.name = name;
         this.methods = methods;
     }
@@ -15,6 +18,12 @@ class LoxClass implements LoxCallable {
     LoxFunction findMethod(String name) {
         if (methods.containsKey(name)) {
             return methods.get(name);
+        }
+
+        // subclasses override methods.  If no method of this name,
+        // check the superclass(es)
+        if (superclass != null) {
+            return superclass.findMethod(name);
         }
         return null;
     }
--- CraftingInterpreters/jlox/com/borkware/lox/Stmt.java
+++ CraftingInterpreters/jlox/com/borkware/lox/Stmt.java
@@ -30,8 +30,9 @@ abstract class Stmt {
     }
 
     static class Class extends Stmt {
-        Class(Token name, List<Stmt.Function> methods) {
+        Class(Token name, Expr.Variable superclass, List<Stmt.Function> methods) {
             this.name = name;
+            this.superclass = superclass;
             this.methods = methods;
         }
 
@@ -41,6 +42,7 @@ abstract class Stmt {
         }
 
         final Token name;
+        final Expr.Variable superclass;
         final List<Stmt.Function> methods;
     }
 
