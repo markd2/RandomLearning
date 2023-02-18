c2844fcd56fa250224982867b9cce8eba575f69d
Mark Dalrymple <markd@borkware.com>
2023-02-18 14:54:51 -0500

    Makefile experimental detritus cleaned up

--- CraftingInterpreters/clox/src/main.c
+++ CraftingInterpreters/clox/src/main.c
@@ -1,12 +1,7 @@
 #include "common.h"
 #include <stdio.h>
 
-void thing1(void);
-void thing2(void);
-
 int main(int argc, const char* argv[]) {
     printf("SNORANG\n");
-    thing1();
-    thing2();
     return 0;
 }
--- CraftingInterpreters/clox/src/thing1.c
+++
@@ -1,4 +0,0 @@
-#include "common.h"
-
-void thing1(void) {
-}
