183bf034d758eb7eb4b4fb511248c545de0b117e
MarkD <markd@borkware.com>
2023-02-20 13:50:18 -0500

    Checkpoint start of Pratt parser.

--- CraftingInterpreters/clox/src/compiler.c
+++ CraftingInterpreters/clox/src/compiler.c
@@ -1,25 +1,80 @@
 #import "compiler.h"
 
 #import <stdio.h>
+#import <stdlib.h>
 
 #import "common.h"
 #import "scanner.h"
 
+typedef struct Parser {
+    Token current;
+    Token previous;
 
-void compile(const char *source) {
-    initScanner(source);
-    int line = -1;
+    bool hadError;
+    bool panicMode;
+} Parser;
+
+static Parser parser;
+
+
+static void errorAt(Token *token, const char *message) {
+    if (parser.panicMode) return;
+    parser.panicMode = true;
+
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    } else if (token->type == TOKEN_ERROR) {
+        // nothing
+    } else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
+    }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+} // errorAt
+
+
+static void errorAtCurrent(const char *message) {
+    errorAt(&parser.current, message);
+} // errorAtCurrent
+
+
+static void error(const char *message) {
+    errorAt(&parser.previous, message);
+} // error
+
+
+static void advance(void) {
+    parser.previous = parser.current;
 
     while (true) {
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        } else {
-            printf("   | ");
-        }
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
 
-        if (token.type == TOKEN_EOF) break;
+        errorAtCurrent(parser.current.start);
     }
+} // advance
+
+
+static void consume(TokenType type, const char *message) {
+    if (parser.current.type == type) {
+        advance();
+        return;
+    }
+
+    errorAtCurrent(message);
+} // consume
+
+bool compile(const char *source, Chunk *chunk) {
+    initScanner(source);
+
+    parser.hadError = false;
+    parser.panicMode = false;
+
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression");
+    return !parser.hadError;
 } // compile
