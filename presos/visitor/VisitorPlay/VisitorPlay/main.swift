b530b4dbd8b442c2f02242f72342c4c6b5c1269b
Mark Dalrymple <markd@bignerdranch.com>
2023-03-08 20:55:05 -0500

    Stubs in some sample visitor code

--- presos/visitor/VisitorPlay/VisitorPlay/main.swift
+++ presos/visitor/VisitorPlay/VisitorPlay/main.swift
@@ -1,11 +1,33 @@
-//
-//  main.swift
-//  VisitorPlay
-//
-//  Created by markd on 3/8/23.
-//
-
 import Foundation
 
-print("Hello, World!")
+struct Expr {
+    let value: Int
+}
 
+enum Token {
+    case add
+    case subtract
+    case multiply
+    case divide
+}
+
+struct Binary {
+    let left: Expr
+    let op: Token
+    let right: Expr
+
+    func evaluateValue() -> Int {
+        switch op {
+        case .add: return left.value + right.value
+        case .subtract: return left.value - right.value
+        case .multiply: return left.value * right.value
+        case .divide: return left.value / right.value
+        }
+    }
+}
+
+
+let blah = Binary(left: Expr(value: 23),
+                  op: .add,
+                  right: Expr(value: 42))
+print(blah.evaluateValue())
