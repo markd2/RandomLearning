d817a523f4e207172f07ae48fca4ac5c4fcacaf0
MarkD <markd@borkware.com>
2023-05-05 12:13:17 -0400

    new project from template

---
+++ SPM/toot1/toot1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toot1
+//
+//  Created by Mark Dalrymple on 5/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
