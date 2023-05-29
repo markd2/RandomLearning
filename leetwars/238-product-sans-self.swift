4a58706a738ab2a3e46f17b4f7596725e54c935e
MarkD <markd@borkware.com>
2023-05-28 21:13:42 -0400

    LC238

---
+++ leetwars/238-product-sans-self.swift
@@ -0,0 +1,12 @@
+#!/usr/bin/swift
+
+// https://leetcode.com/problems/product-of-array-except-self/
+// given array of numbers, return array of products of the nubmers,
+// element i should not be included in the product
+
+
+import Foundation
+
+class Solution {
+}
+
