1ecc5cffd2a21774c9d8d22e9a5e4a8b160f714a
Mark Dalrymple <markda@zillowgroup.com>
2023-07-27 16:31:53 -0400

    adds new projet (correctly this time)

---
+++ CoreData/1-SplungeData/1-SplungeData/__SplungeDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  __SplungeDataApp.swift
+//  1-SplungeData
+//
+//  Created by Mark Dalrymple on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct __SplungeDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
