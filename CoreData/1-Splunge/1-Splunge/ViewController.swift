ed1f0247a069c72b71ef71a1a28775b60c0c4264
Mark Dalrymple <markda@zillowgroup.com>
2023-07-27 17:10:06 -0400

    one more time

---
+++ CoreData/1-Splunge/1-Splunge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1-Splunge
+//
+//  Created by Mark Dalrymple on 7/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
