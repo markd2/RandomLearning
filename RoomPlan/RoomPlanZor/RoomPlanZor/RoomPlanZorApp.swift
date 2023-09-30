1719ca867573c4292a32620edc608c56c69dade3
Mark Dalrymple <markd@borkware.com>
2023-09-30 18:16:03 -0400

    new project from template

---
+++ RoomPlan/RoomPlanZor/RoomPlanZor/RoomPlanZorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomPlanZorApp.swift
+//  RoomPlanZor
+//
+//  Created by markd on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct RoomPlanZorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
