786e2c323bc59c99aa0fd01bb14ecf8a4f8ea089
Mark Dalrymple <markd@borkware.com>
2023-09-30 19:53:54 -0400

    Adds the placeholder UIViewRepresentable

--- RoomPlan/RoomPlanZor/RoomPlanZor/SimplePlan.swift
+++ RoomPlan/RoomPlanZor/RoomPlanZor/SimplePlan.swift
@@ -7,9 +7,29 @@
 
 import SwiftUI
 
+//  When you want your view to coordinate with other SwiftUI views,
+//  you must provide a Coordinator instance to facilitate those
+//  interactions. For example, you use a coordinator to forward
+//  target-action and delegate messages from your view to any SwiftUI
+//  views.
+
+struct PlaceholderContainerView: UIViewRepresentable {
+
+    func makeUIView(context: Context) -> PlaceholderUIView {
+        return PlaceholderUIView()
+    }
+
+    func updateUIView(_ uIView: PlaceholderUIView, context: Context) {
+        print("update ui view \(context)")
+    }
+}
+
 struct SimplePlan: View {
     var body: some View {
-        Text("Simple Plan!")
+        VStack {
+            Text("Snornge")
+            PlaceholderContainerView()
+        }
     }
 }
 
