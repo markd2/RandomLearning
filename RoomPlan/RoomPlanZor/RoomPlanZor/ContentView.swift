88dfb49bead7c8ad5efba1753dff6a80b0eafe21
Mark Dalrymple <markd@borkware.com>
2023-10-02 20:50:12 -0400

    Default to middle tab.

--- RoomPlan/RoomPlanZor/RoomPlanZor/ContentView.swift
+++ RoomPlan/RoomPlanZor/RoomPlanZor/ContentView.swift
@@ -8,21 +8,34 @@
 import SwiftUI
 import RoomPlan
 
+enum Tab: Hashable {
+    case simplePlan
+    case complicatedPlan
+    case multiPlan
+
+    var id: Self { self }
+}
+
 struct ContentView: View {
+    @State private var tab = Tab.complicatedPlan
+
     var body: some View {
-        TabView {
+        TabView(selection: $tab) {
             SimplePlan(captureModel: SimpleCaptureModel.shared)
               .tabItem {
                   Label("Simple Plan", systemImage: "house")
               }
+              .tag(Tab.simplePlan)
             CompliPlan(captureModel: CompliCaptureModel.shared)
               .tabItem {
                   Label("Complicated Plan", systemImage: "house.circle.fill")
               }
+              .tag(Tab.complicatedPlan)
             MultiPlan()
               .tabItem {
                   Label("Multi Plan", systemImage: "house.lodge")
               }
+              .tag(Tab.multiPlan)
         }
     }
 }
