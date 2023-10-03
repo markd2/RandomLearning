70770d6d4ca8d23ec0e8825e232a5a8794416181
Mark Dalrymple <markd@borkware.com>
2023-10-02 22:01:34 -0400

    Epic fail trying to export a model.

--- RoomPlan/RoomPlanZor/RoomPlanZor/SimplePlan.swift
+++ RoomPlan/RoomPlanZor/RoomPlanZor/SimplePlan.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import RoomPlan
 
 
-class SimpleCaptureModel: ObservableObject {
+class SimpleCaptureModel: ObservableObject, NSCoding {
     var roomCaptureView: RoomCaptureView
     var captureSessionConfig: RoomCaptureSession.Configuration
 
@@ -21,7 +21,13 @@ class SimpleCaptureModel: ObservableObject {
         self.captureSessionConfig = captureSessionConfig
     }
 
+    // The room capture view delegate requires NSCoding (why...)
+    // and can't have a required initializer in an extension.
+    func encode(with coder: NSCoder) { }
+    required init?(coder: NSCoder) { fatalError("why is this NSCoding?") }
+
     func startSession() {
+        roomCaptureView.delegate = self
         roomCaptureView.captureSession.run(configuration: captureSessionConfig)
         sessionRunning = true
     }
@@ -38,6 +44,39 @@ class SimpleCaptureModel: ObservableObject {
                                               captureSessionConfig: captureSessionConfig)
         return captureModel
     }()
+}
+
+extension SimpleCaptureModel: RoomCaptureViewDelegate {
+    // let the user play with it.
+    func captureView(shouldPresent roomDataForProcessing: CapturedRoomData,
+                     error: (Error)?) -> Bool {
+        return true
+    }
+
+    func generateCurrentTimeStamp () -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy_MM_dd_hh_mm_ss"
+        return (formatter.string(from: Date()) as NSString) as String
+    }
+    
+    func captureView(didPresent processedResult: CapturedRoom, error: (Error)?) {
+        let fm = FileManager()
+        do {
+            let documentDirectoryURL = try fm.url(for: .documentDirectory, 
+                                                  in: .userDomainMask, 
+                                                  appropriateFor: nil, 
+                                                  create: true)
+            let now = generateCurrentTimeStamp()
+            let destinationURL = documentDirectoryURL.appendingPathComponent("\(now).usdz")
+            try processedResult.export(to: destinationURL,
+                                       exportOptions: .model)
+        } catch {
+            // https://developer.apple.com/forums/thread/738801 (awaiting moderation)
+            // (sigh) Warning: in SdfPath at line 151 of sdf/path.cpp -- Ill-formed SdfPath </2023_10_02_09_23_23>: syntax error - Coding Error: in _IsValidPathForCreatingPrim at line 3338 of usd/stage.cpp -- Path must be an absolute path: <>
+
+            print("oops no processed result? \(error)")
+        }
+    }
 }
 
 struct SimplePlan: View {
