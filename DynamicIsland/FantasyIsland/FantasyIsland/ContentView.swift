2e3f3f0bedacad01f6719357c32b1767deb365b8
Mark Dalrymple <markd@borkware.com>
2023-10-22 11:37:32 -0400

    Thoroughly customized the interface

--- DynamicIsland/FantasyIsland/FantasyIsland/ContentView.swift
+++ DynamicIsland/FantasyIsland/FantasyIsland/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "airplane")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Bozz.  the plane! the plane!!")
         }
         .padding()
     }
