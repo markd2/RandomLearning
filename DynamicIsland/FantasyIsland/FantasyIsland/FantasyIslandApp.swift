ba65ae8ba8b517d56a8aebb3d259df1db2b4444f
Mark Dalrymple <markd@borkware.com>
2023-10-22 11:33:56 -0400

    adds dynamic island playpen

---
+++ DynamicIsland/FantasyIsland/FantasyIsland/FantasyIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  FantasyIslandApp.swift
+//  FantasyIsland
+//
+//  Created by markd on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct FantasyIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
