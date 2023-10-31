fc5aad75ed2e9c1ce41c50c50503dea216b738c7
Mark Dalrymple <markda@zillowgroup.com>
2023-10-31 13:59:15 -0400

    Adds some comments on what each part of the splunge sample are doing.

--- C++/interop/Splunge/Splunge/main.swift
+++ C++/interop/Splunge/Splunge/main.swift
@@ -12,14 +12,19 @@ import HappyFunTime
 
 print("Snorgle!")
 
+// call a simple C++ function
 let blah = cxxFunction(7)
 print(blah)
 
+// Use a C++ enum from a pure C++ ibrary target
 let larch = Tree(.Oak)
 
 var greeble: TreeKind = .Oak
 print(greeble, greeble.rawValue)
 
+// HappyFunTime is a framework target with mixed swift and C++ project
+// The HappyFun / FunHappy calculate fibonacci by bouncing between
+// languages
 let snork = HappyFunCplusplus(true) // C++
 let ook = snork.happyfun(10)
 print(ook)
@@ -28,8 +33,8 @@ let snork2 = FunHappy(printInvocation: true) // Swift
 let ook2 = snork2.happyfun(10)
 print(ook2)
 
+// play with some templates and explicit specialization on the C++ side
 let oop = getMagicNumber()
 print(oop)
 
 let greeble2 = CharCharFraction(2, 10);
-
