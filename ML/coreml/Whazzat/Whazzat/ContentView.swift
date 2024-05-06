93f5048bdddb4ab733702b8803c9c478d45854cd
Mark Dalrymple <markda@zillowgroup.com>
2024-05-06 11:27:48 -0400

    Use features without going through generated interface. not that hard. tbh

--- ML/coreml/Whazzat/Whazzat/ContentView.swift
+++ ML/coreml/Whazzat/Whazzat/ContentView.swift
@@ -10,19 +10,64 @@ import UIKit
 import CoreML
 
 class Helperer {
-    let model: Inceptionv3
+    let iv3: Inceptionv3
     
     init() {
         let configuration = MLModelConfiguration()
-        model = try! Inceptionv3(configuration: configuration)
+        iv3 = try! Inceptionv3(configuration: configuration)
     }
     
     func checkBöb() -> String {
 //        let böburl = Bundle.main.url(forResource: "bob-with-lego-car", withExtension: "jpg")!
         let böburl = Bundle.main.url(forResource: "sleesstak", withExtension: "jpg")!
         let input = try! Inceptionv3Input(imageAt: böburl)
-        let prediction = try! model.prediction(input: input)
+        let prediction = try! iv3.prediction(input: input)
         return prediction.classLabel
+    }
+    
+    func checkBob(_ imageName: String) -> String {
+        let rawModel: MLModel = iv3.model
+        
+        class FeatureProvider: MLFeatureProvider {
+            let imageName: String
+       
+            init(imageName: String) {
+                self.imageName = imageName
+            }
+            
+            var featureNames: Set<String> {
+                get {
+                    ["image"]
+                }
+            }
+            
+            func featureValue(for featureName: String) -> MLFeatureValue? {
+                switch featureName {
+                case "image":
+                    let böburl = Bundle.main.url(forResource: imageName, 
+                                                 withExtension: "jpg")!
+                    
+                    let imageFeatureValue = try! MLFeatureValue(
+                        imageAt: böburl,
+                        pixelsWide: 299,
+                        pixelsHigh: 299,
+                        pixelFormatType: kCVPixelFormatType_32ARGB,
+                        options: nil
+                    )
+                    return imageFeatureValue
+                default:
+                    print("omgwtfbbq?")
+                    return nil
+                }
+            }
+
+        }
+        
+        let provider = FeatureProvider(imageName: imageName)
+        let result = try! rawModel.prediction(from: provider)
+        let featureValue = result.featureValue(for: "classLabel")
+        let stringValue = featureValue?.stringValue
+        return stringValue!
     }
 }
 
@@ -37,10 +82,13 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-            Button("Splunge") {
+            Button("Splunge with Help") {
                 print(Helperer().checkBöb())
             }
-        }
+             Button("Splunge without Help") {
+                print(Helperer().checkBob("sleesstak"))
+            }
+       }
         .padding()
     }
 }
