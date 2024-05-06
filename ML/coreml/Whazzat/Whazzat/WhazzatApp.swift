776004142d84b5e3af911f65bb03c5309adad81b
Mark Dalrymple <markda@zillowgroup.com>
2024-05-06 09:34:16 -0400

    new project from template

---
+++ ML/coreml/Whazzat/Whazzat/WhazzatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhazzatApp.swift
+//  Whazzat
+//
+//  Created by Mark Dalrymple on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WhazzatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
