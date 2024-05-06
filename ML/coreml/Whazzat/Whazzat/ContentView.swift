9136c9ff6e037ccf10ec1ce7c33592a3debcab59
Mark Dalrymple <markda@zillowgroup.com>
2024-05-06 10:03:11 -0400

    Basic recognition

--- ML/coreml/Whazzat/Whazzat/ContentView.swift
+++ ML/coreml/Whazzat/Whazzat/ContentView.swift
@@ -6,14 +6,40 @@
 //
 
 import SwiftUI
+import UIKit
+import CoreML
+
+class Helperer {
+    let model: Inceptionv3
+    
+    init() {
+        let configuration = MLModelConfiguration()
+        model = try! Inceptionv3(configuration: configuration)
+    }
+    
+    func checkBöb() -> String {
+//        let böburl = Bundle.main.url(forResource: "bob-with-lego-car", withExtension: "jpg")!
+        let böburl = Bundle.main.url(forResource: "sleesstak", withExtension: "jpg")!
+        let input = try! Inceptionv3Input(imageAt: böburl)
+        let prediction = try! model.prediction(input: input)
+        return prediction.classLabel
+    }
+}
 
 struct ContentView: View {
+
+    func splunge() {
+    }
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button("Splunge") {
+                print(Helperer().checkBöb())
+            }
         }
         .padding()
     }
