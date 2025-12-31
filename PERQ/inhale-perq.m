a263d1c8a421da715689f04849068fd5548171cb
MarkD <markd@borkware.com>
2025-12-30 20:47:47 -0500

    convert perq-generated ascii file into binary

---
+++ PERQ/inhale-perq.m
@@ -0,0 +1,50 @@
+#import <Foundation/Foundation.h>
+#import <stdio.h>
+
+// inhale-perq.m - go from ascii interchange format to a binary file
+// be sure to convert newlines with mac2unix
+
+// clang -g -Wall -framework Foundation -o inhale-perq inhale-perq.m
+
+
+// file format is ascii, one byte (decimal) per line.
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "%s filename - inhales file (ascii) and emits binary\n",
+                argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        fprintf(stderr, "could not open %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    char buffer[666];
+    size_t amountRead;
+
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        char blah = (char)atoi(buffer);
+        putchar(blah);
+        // printf("got %hhx\n", blah);
+    }
+
+#if 0
+    while (!feof(file) && !ferror(file)) {
+        amountRead = fread(buffer, 
+                           sizeof(char),                  // size of item
+                           sizeof(buffer) / sizeof(char), // number of items
+                           file);
+        if (amountRead == sizeof(buffer)) {
+            fprintf(stderr, "got unexpectedly long line.  run mac2unix on the file to fix newlines\n");
+            return EXIT_FAILURE;
+        }
+        printf("grooby read %zu -  %s\n", amountRead, buffer);
+    }
+#endif
+
+    return EXIT_SUCCESS;
+
+} // main
