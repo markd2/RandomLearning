cbf9768cdeb7cdf5f2f17cc662bdd971607db696
MarkD <markd@borkware.com>
2026-03-24 21:44:35 -0400

    got segments reacting to clicks

--- PAL-1/KIM-LED-MAP/KIM-LED-MAP/AppDelegate.m
+++ PAL-1/KIM-LED-MAP/KIM-LED-MAP/AppDelegate.m
@@ -1,32 +1,47 @@
-//
-//  AppDelegate.m
-//  KIM-LED-MAP
-//
-//  Created by markd on 3/24/26.
-//
+// AppDelegate.m - the, uh, app delegate.
 
 #import "AppDelegate.h"
+#import <AppKit/AppKit.h>
+
+
+@interface ClickableImageView: NSImageView
+@end // ClickableImageView
+
+
+@implementation ClickableImageView
+
+- (void) mouseDown: (NSEvent *) event {
+    [self sendAction: self.action  to: nil];
+} // mouseDown
+
+@end // ClickableImageView
+
 
 @interface AppDelegate ()
 
 @property (strong) IBOutlet NSWindow *window;
-@end
+
+@property (strong) IBOutlet ClickableImageView *segment0;
+@property (strong) IBOutlet ClickableImageView *segment1;
+@property (strong) IBOutlet ClickableImageView *segment2;
+@property (strong) IBOutlet ClickableImageView *segment3;
+@property (strong) IBOutlet ClickableImageView *segment4;
+@property (strong) IBOutlet ClickableImageView *segment5;
+@property (strong) IBOutlet ClickableImageView *segment6;
+
+@property (assign) char segmentMask;
+@end// extension
+
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
-}
-
-
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
-}
-
-
-- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+- (BOOL) applicationSupportsSecureRestorableState: (NSApplication *) app {
     return YES;
-}
+} // applicationSupportsSecureRestorableState
 
 
-@end
+- (IBAction) segmentClicked: (ClickableImageView *) sender {
+    NSLog(@"OOGIE");
+} // segmentClicked
+
+@end // AppDelegate
